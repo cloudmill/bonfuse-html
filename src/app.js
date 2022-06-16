9c2e5af421bfc5db36ef8224316f35f63569f545
Kirill Matveev <dejar@protonmail.com>
2022-06-16 19:19:41 +0300

    worked on the animations

--- src/app.js
+++ src/app.js
@@ -1,5 +1,27 @@
 import "./styles/app.scss";
 
-import "./scripts/test";
-import "./scripts/swiper-test";
-import "./scripts/typescript-test.ts";
+document.addEventListener('DOMContentLoaded', () => {
+  
+
+})
+
+window.addEventListener('load', () => {
+
+  document.querySelector('.body').classList.remove('no-transition');
+
+  document.querySelector('.plug-page__logo').classList.add('loaded');
+
+  setTimeout(() => {
+
+    document.querySelector('.plug-page__top').classList.add('loaded');
+    document.querySelector('.plug-page__bottom').classList.add('loaded');
+
+  }, 600);
+
+  setTimeout(() => {
+
+    document.querySelector('.balloon.b2').classList.add('start');
+    
+  }, 5000);
+
+})
